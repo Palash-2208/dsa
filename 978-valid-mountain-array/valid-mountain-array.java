dfe08e42747d3fa3108ce43d7028b732484ae2c7
B Palash <bpalash2208@gmail.com>
2025-08-24 14:53:37 +0530

    Time: 1 ms (99.33%) | Memory: 45.4 MB (57.76%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int n = arr.length;
+        if (n<3) return false;
+        int peak=0;
+        for (int i=1;i< n;i++) {
+            if (arr[i] > arr[peak]) {
+                peak = i;
+            }
+        }
+        if (peak==0||peak==n - 1) return false;
+        for (int i=0; i<peak;i++) {
+            if (arr[i] >= arr[i + 1]) return false;
+        }
+        for (int i=peak;i< n - 1; i++) {
+            if (arr[i] <= arr[i + 1]) return false;
+        }
+
+        return true;
+    }
+}
